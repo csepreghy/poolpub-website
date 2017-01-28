03c3fed8ca1afae9189d547175946cc4e6a897b4
Andras Csepreghy <acsepreghy@ganintegrity.com>
2017-01-28 21:09:40 +0100

    Navbar in place, font is added to the site

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,14 @@ class NavBar extends Component {
   render() {
     return (
       <div id="navbar-container">
-
+        <ul>
+          <li>Menu</li>
+          <li>Gallery</li>
+          <li>Contact</li>
+          <li>Party Room</li>
+          <li>Prices</li>
+          <li>About us</li>
+        </ul>
       </div>
     );
   }
