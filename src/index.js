8661127bbb370b6a1c514b5b665a8f223224f373
Andras Csepreghy <acsepreghy@ganintegrity.com>
2017-02-23 15:14:41 +0100

    removed unnecessary routing

--- src/index.js
+++ src/index.js
@@ -7,14 +7,9 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import { Router, Route, browserHistory } from 'react-router';
 import PhotoGallery from './PhotoGallery';
 import PartyRoom from './PartyRoom';
 
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-    <Route path="gallery" component={PhotoGallery} />
-    <Route path="party-room" component={PartyRoom} />
-  </Router>
+  <App />
 ), document.getElementById('the-universe'));
