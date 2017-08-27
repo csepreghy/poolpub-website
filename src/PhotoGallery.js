0f86373cf00fb140266a0e083cd1c17005b31cba
Andras Csepreghy <acsepreghy@ganintegrity.com>
2017-08-27 11:56:07 +0200

    Redesigned gallery

--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Gallery from 'react-photo-gallery';
 import { TEXT } from './TEXT';
+import { PHOTO_SET } from './photoSet.js';
 
 class PhotoGallery extends Component {
 
@@ -8,302 +9,27 @@ class PhotoGallery extends Component {
     super();
 
     this.state = {
+      isGalleryVisible: false
     }
+
+    this.toggleGallery = this.toggleGallery.bind(this);
   }
 
-  componentDidMount() {
+  toggleGallery() {
+    this.setState({ isGalleryVisible: !this.state.isGalleryVisible });  
   }
-
-  seeMoreToggle() {
-    if (this.state.heightClassName === "h-100vh") {
-      this.setState({ heightClassName: "h-380vh" });
-      setTimeout(() => this.setAutoHeight(), 1100);
-    } else if (this.state.heightClassName === "auto" || this.state.heightClassName === "h-100vh") {
-      this.setState({ heightClassName: "h-100vh" });
-    }
-  }
-
-  TEXT = TEXT.default;
-
-  PHOTO_SET = [
-    {
-      src: 'assets/gallery/400/a1.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a1.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a2.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a2.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a3.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a3.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a4.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a4.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a5.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a5.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a6.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a6.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a7.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a7.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a8.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a8.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a9.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a9.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a10.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a10.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a11.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a11.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a12.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a12.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a13.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a13.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a14.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a14.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a15.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a15.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a16.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a16.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a17.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a17.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a18.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a18.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a19.jpg',
-      width: 1200,
-      height: 1800,
-      aspectRatio: 0.66,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a19.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a20.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a20.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a21.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a21.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a22.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a22.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a23.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a23.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a24.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a24.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a26.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a26.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a27.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a27.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a28.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a28.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a29.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a29.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a30.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a30.jpg',
-      }
-    },
-    {
-      src: 'assets/gallery/400/a31.jpg',
-      width: 1800,
-      height: 1200,
-      aspectRatio: 1.5,
-      lightboxImage:{
-        src: 'assets/gallery/1800/a31.jpg',
-      }
-    }
-  ];
 
   render() {
     return (
-      <section id="gallery" className="block ">
-        <div className="gallery-container" style={{ "width": this.state.width }}>
-          { /* <Gallery photos={ this.PHOTO_SET }/> */ }
-          <div className="open-gallery-btn">{ TEXT.photoGallery.openGalleryBtn[this.props.lang] }</div>
+      <section id="gallery" className="block">
+        <div className="gallery-container" style={{ 'width': this.state.width }}>
+          <div className="gallery-modal" style={{'display': this.state.isGalleryVisible ? 'block' : 'none'}}>
+            <Gallery photos={ PHOTO_SET } cols={7}/>
+          </div>
+          <div onClick={ this.toggleGallery } 
+               className="close-gallery-btn" 
+               style={{'display': this.state.isGalleryVisible ? 'block' : 'none'}}><img src="/assets/close.png" /></div>
+          <div onClick={ this.toggleGallery } className="open-gallery-btn">{ TEXT.photoGallery.openGalleryBtn[this.props.lang] }</div>
         </div>
       </section>
     );
---
+++ src/photoSet.js
@@ -0,0 +1,722 @@
+export const PHOTO_SET = [
+  {
+    src: 'assets/gallery/250/a1.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a1.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a2.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a2.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a3.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a3.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a4.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a4.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a5.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a5.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a6.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a6.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a7.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a7.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a8.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a8.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a9.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a9.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a10.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a10.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a11.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a11.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a12.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a12.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a13.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a13.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a14.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a14.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a15.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a15.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a16.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a16.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a17.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a17.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a19.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a19.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a20.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a20.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a21.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a21.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a22.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a22.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a23.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a23.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a24.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a24.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a25.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a25.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a26.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a26.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a27.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a27.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a28.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a28.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a29.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a29.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a30.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a30.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a31.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a31.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a32.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a32.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a33.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a33.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a34.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a34.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a35.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a35.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a36.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a36.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a37.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a37.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a38.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a38.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a39.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a39.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a40.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a40.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a41.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a41.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a42.jpg',
+    width: 1200,
+    height: 1800,
+    aspectRatio: 0.66,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a42.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a43.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a43.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a44.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a44.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a45.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a45.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a46.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a46.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a47.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a47.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a48.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a48.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a49.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a49.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a50.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a50.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a51.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a51.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a52.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a52.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a53.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a53.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a54.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a54.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a55.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a55.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a56.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a56.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a57.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a57.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a58.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a58.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a59.jpg',
+    width: 1200,
+    height: 1800,
+    aspectRatio: 0.66,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a59.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a60.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a60.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a61.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a61.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a62.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a62.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a63.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a63.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a64.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a64.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a65.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a65.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a66.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a66.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a67.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a67.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a68.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a68.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a69.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a69.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a70.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a70.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a71.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a71.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a72.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a72.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a73.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a73.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a74.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a74.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a75.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a75.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a76.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a76.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a77.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a77.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a78.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a78.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a79.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a79.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a80.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a80.jpg'
+    }
+  },
+  {
+    src: 'assets/gallery/250/a81.jpg',
+    width: 1800,
+    height: 1200,
+    aspectRatio: 1.5,
+    lightboxImage: {
+      src: 'assets/gallery/1800/a81.jpg'
+    }
+  }
+]
